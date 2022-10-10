454b29931c25f685604b8fc8a6affcd17867507a
Alexis Guzman <alexis12989x@gmail.com>
2022-10-10 17:09:18 -0500

    feat(header): setup item component

--- src/components/layout/header/Item.tsx
+++ src/components/layout/header/Item.tsx
@@ -1,8 +1,19 @@
-const Item = () => {
+import { FC, ReactNode } from 'react';
+
+import { Link } from '~/components';
+
+interface ItemProps {
+  href: string;
+  children: ReactNode;
+}
+
+const Item: FC<ItemProps> = ({ href, children }) => {
   return (
-    <div>
-      <p>Item Component</p>
-    </div>
+    <li>
+      <Link href={href} isExternal>
+        {children}
+      </Link>
+    </li>
   );
 };
 
