39d8ae267fcb53fc4af8d9f53d46dd1b87d47755
Alexis Guzman <alexis12989x@gmail.com>
2022-10-10 13:59:55 -0500

    feat(components): setup link component

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,8 +1,24 @@
-const Link = () => {
+import NextLink from 'next/link';
+import { FC, ReactNode } from 'react';
+
+interface LinkProps {
+  isExternal: true;
+  className: string;
+  href: string;
+  children: ReactNode;
+}
+
+const Link: FC<LinkProps> = ({ isExternal, className, href, children }) => {
   return (
-    <div>
-      <p>Link Component</p>
-    </div>
+    <NextLink href={href} className={className}>
+      {isExternal ? (
+        <a rel='noopener noreferrer' target='_blank'>
+          {children}
+        </a>
+      ) : (
+        <a>{children}</a>
+      )}
+    </NextLink>
   );
 };
 
