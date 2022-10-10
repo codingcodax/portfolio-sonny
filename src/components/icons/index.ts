9777659aea4028cb0543004c71c65b02756d9b10
Alexis Guzman <alexis12989x@gmail.com>
2022-10-10 13:22:23 -0500

    feat(icons): export twitter icon

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as FacebookIcon } from './Facebook';
 export { default as InstagramIcon } from './Instagram';
+export { default as TwitterIcon } from './Twitter';
 export { default as YoutubeIcon } from './Youtube';
