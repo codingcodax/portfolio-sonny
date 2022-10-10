81d88dfa143f3ba789fb7381dd5b26b88c5456a7
Alexis Guzman <alexis12989x@gmail.com>
2022-10-10 13:28:17 -0500

    fix(icons): remove height and width props

--- src/components/icons/Twitter.tsx
+++ src/components/icons/Twitter.tsx
@@ -7,8 +7,6 @@ const Twitter: FC<IconType> = ({ className }) => {
     <svg
       xmlns='http://www.w3.org/2000/svg'
       className={className}
-      width='24'
-      height='24'
       viewBox='0 0 24 24'
       stroke='currentColor'
       fill='none'
