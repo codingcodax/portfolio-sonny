378c773a10c4f7ec5bcf47ceb21fc5144a4a2777
Alexis Guzman <alexis12989x@gmail.com>
2022-10-10 14:04:42 -0500

    feat(home): export header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import type { NextPage } from 'next';
 
+import { Header } from '~/components/layout';
+
 const Home: NextPage = () => {
   return (
     <div>
+      <Header />
       <p>Home Page</p>
     </div>
   );
