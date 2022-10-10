ddee9b2fb9bc5c8f9312a245ab82b6c5a1e435a0
Alexis Guzman <alexis12989x@gmail.com>
2022-10-10 13:33:38 -0500

    feat(icons): import icon type

--- src/components/icons/Mail.tsx
+++ src/components/icons/Mail.tsx
@@ -1,4 +1,8 @@
-const Mail = ({ className }) => {
+import { FC } from 'react';
+
+import type { IconType } from '~/@types';
+
+const Mail: FC<IconType> = ({ className }) => {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
