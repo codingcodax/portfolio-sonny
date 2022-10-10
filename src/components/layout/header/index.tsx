be2ad2e975f91a6ec2df6947e06e4211f7fc5cf0
Alexis Guzman <alexis12989x@gmail.com>
2022-10-10 17:10:29 -0500

    feat(layout): setup header component

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,8 +1,20 @@
+import { Link } from '~/components';
+import { MailIcon } from '~/components/icons';
+
+import Nav from './Nav';
+
 const Header = () => {
   return (
-    <div>
-      <p>Header Component</p>
-    </div>
+    <header className='p-5 mx-auto max-w-7xl sticky flex justify-between'>
+      <Nav />
+
+      <Link href='/' className='flex items-center gap-2'>
+        <MailIcon className='h-8 w-8 stroke-gray-400' />
+        <span className='hidden uppercase text-sm text-gray-400 md:inline-block'>
+          Get In Touch
+        </span>
+      </Link>
+    </header>
   );
 };
 
