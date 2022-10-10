a2eed245472f26fb2cbe69bb1259a4211fadad8e
Alexis Guzman <alexis12989x@gmail.com>
2022-10-10 17:08:23 -0500

    feat(components): make is external and classname props opcional

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -2,21 +2,26 @@ import NextLink from 'next/link';
 import { FC, ReactNode } from 'react';
 
 interface LinkProps {
-  isExternal: true;
-  className: string;
+  isExternal?: true;
+  className?: string;
   href: string;
   children: ReactNode;
 }
 
-const Link: FC<LinkProps> = ({ isExternal, className, href, children }) => {
+const Link: FC<LinkProps> = ({
+  isExternal = false,
+  className,
+  href,
+  children,
+}) => {
   return (
-    <NextLink href={href} className={className}>
+    <NextLink href={href}>
       {isExternal ? (
-        <a rel='noopener noreferrer' target='_blank'>
+        <a rel='noopener noreferrer' target='_blank' className={className}>
           {children}
         </a>
       ) : (
-        <a>{children}</a>
+        <a className={className}>{children}</a>
       )}
     </NextLink>
   );
