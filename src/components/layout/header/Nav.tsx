514013f2dee7244a3410e7ca53de114f54656c9e
Alexis Guzman <alexis12989x@gmail.com>
2022-10-10 17:09:46 -0500

    feat(header): setup header component

--- src/components/layout/header/Nav.tsx
+++ src/components/layout/header/Nav.tsx
@@ -1,8 +1,24 @@
+import { InstagramIcon, TwitterIcon, YoutubeIcon } from '~/components/icons';
+
+import Item from './Item';
+
 const Nav = () => {
   return (
-    <div>
-      <p>Nav Component</p>
-    </div>
+    <nav>
+      <ul className='flex gap-4'>
+        <Item href='https://www.youtube.com/channel/UCMY0GhV1HuX4XdbgalC77VQ'>
+          <YoutubeIcon className='h-8 w-8 stroke-gray-400' />
+        </Item>
+
+        <Item href='https://www.instagram.com/codingcodax/'>
+          <InstagramIcon className='h-8 w-8 stroke-gray-400' />
+        </Item>
+
+        <Item href='https://twitter.com/codingcodax'>
+          <TwitterIcon className='h-8 w-8 stroke-gray-400' />
+        </Item>
+      </ul>
+    </nav>
   );
 };
 
