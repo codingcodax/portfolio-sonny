90d5ab5de01fbdacf75443d03d5ac2fe070236c3
Alexis Guzman <alexis12989x@gmail.com>
2022-10-11 14:07:43 -0500

    feat(home): setup hero component

--- src/components/home/hero/index.tsx
+++ src/components/home/hero/index.tsx
@@ -1,6 +1,8 @@
+import Image from 'next/image';
 import { Cursor, useTypewriter } from 'react-simple-typewriter';
 
 import Circles from './Circles';
+import Links from './links';
 
 const Hero = () => {
   const [text] = useTypewriter({
@@ -16,10 +18,29 @@ const Hero = () => {
   return (
     <div className='h-screen flex items-center justify-center flex-col space-y-8 text-center snap-center overflow-hidden'>
       <Circles />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor='#f7ab0a' />
-      </h1>
+
+      <div>
+        <Image
+          alt='Alexis'
+          className='rounded-full'
+          height={128}
+          objectFit='cover'
+          src='https://media-exp1.licdn.com/dms/image/C4E03AQEX_9Lp5RfjuQ/profile-displayphoto-shrink_800_800/0/1610992691122?e=1671062400&v=beta&t=wxtVwsIGZShz83FKzZ42Fbh4fFi-2WWVFbjO0xYKGrU'
+          width={128}
+        />
+      </div>
+
+      <div className='z-20'>
+        <h2 className='mb-2 text-gray-500 text-sm uppercase tracking-[15px]'>
+          Frontend Developer
+        </h2>
+        <h1 className='mb-5 text-5xl lg:text-6xl font-semibold scroll-px-10'>
+          <span className='mr-3'>{text}</span>
+          <Cursor cursorColor='#f7ab0a' />
+        </h1>
+
+        <Links />
+      </div>
     </div>
   );
 };
