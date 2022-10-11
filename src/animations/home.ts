0b77e3d3196ed357b448e445087bf7b084102d26
Alexis Guzman <alexis12989x@gmail.com>
2022-10-11 10:54:38 -0500

    feat(animations): create circles animations

---
+++ src/animations/home.ts
@@ -0,0 +1,9 @@
+export const circlesAnimation = {
+  hidden: { opacity: 0 },
+  show: {
+    scale: [1, 2, 2, 3, 1],
+    opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1],
+    borderRadius: ['20%', '20%', '50%', '80%', '20%'],
+    transition: { duration: 2.5 },
+  },
+};
