954708fe59c55f2a166e4687bbfcc67764759ef6
Alexis Guzman <alexis12989x@gmail.com>
2022-10-11 14:05:06 -0500

    feat(hero): setup links component

--- src/components/home/hero/links/index.tsx
+++ src/components/home/hero/links/index.tsx
@@ -1,7 +1,12 @@
+import Item from './Item';
+
 const Links = () => {
   return (
-    <div>
-      <p>Links Component</p>
+    <div className='flex justify-center gap-x-2'>
+      <Item href='#about'>About</Item>
+      <Item href='#experience'>Experience</Item>
+      <Item href='#skills'>Skills</Item>
+      <Item href='#projects'>Projects</Item>
     </div>
   );
 };
