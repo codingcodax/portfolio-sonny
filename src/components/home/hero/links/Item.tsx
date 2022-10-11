be8dc4c4783bc3a5c5e96a77427bb8675008382a
Alexis Guzman <alexis12989x@gmail.com>
2022-10-11 14:04:33 -0500

    feat(links): setup item component

--- src/components/home/hero/links/Item.tsx
+++ src/components/home/hero/links/Item.tsx
@@ -1,8 +1,20 @@
-const Item = () => {
+import { FC, ReactNode } from 'react';
+
+import Link from '~/components/Link';
+
+interface ItemProps {
+  href: string;
+  children: ReactNode;
+}
+
+const Item: FC<ItemProps> = ({ href, children }) => {
   return (
-    <div>
-      <p>Item Component</p>
-    </div>
+    <Link
+      className='py-2 px-6 text-gray-500 text-xs uppercase border border-[#242424] rounded-full tracking-widest transition-all hover:border-[#f7ab0a]/40 hover:text-[#f7ab0a]/40'
+      href={href}
+    >
+      {children}
+    </Link>
   );
 };
 
