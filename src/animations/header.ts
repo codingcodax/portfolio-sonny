49aa5d7f573ac37e670607879c853d82d08e2256
Alexis Guzman <alexis12989x@gmail.com>
2022-10-11 09:15:27 -0500

    feat(animations): create header animations

---
+++ src/animations/header.ts
@@ -0,0 +1,9 @@
+export const comingRight = {
+  hidden: { x: -500, opacity: 0, scale: 0.5 },
+  show: { x: 0, opacity: 1, scale: 1, transition: { duration: 1.5 } },
+};
+
+export const comingLeft = {
+  hidden: { x: 500, opacity: 0, scale: 0.5 },
+  show: { x: 0, opacity: 1, scale: 1, transition: { duration: 1.5 } },
+};
