7a043ea92858cff1e0c2c021adf60e4e80c35fca
Alexis Guzman <alexis12989x@gmail.com>
2022-10-11 10:45:15 -0500

    feat(pages): add tailwindcss styles for snap into home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import { Hero } from '~/components/home';
 
 const Home: NextPage = () => {
   return (
-    <div className='h-screen text-white bg-[rgb(36,36,36)]'>
+    <div className='h-screen text-white bg-[rgb(36,36,36)] snap-y snap-mandatory overflow-scroll z-0'>
       <Header />
       <Hero />
     </div>
