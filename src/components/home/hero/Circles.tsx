a622ed04784b982ad5fe3e4521c63e440ef2cea4
Alexis Guzman <alexis12989x@gmail.com>
2022-10-11 10:55:25 -0500

    feat(hero): convert into a motion component and animate with circles animation

--- src/components/home/hero/Circles.tsx
+++ src/components/home/hero/Circles.tsx
@@ -1,12 +1,21 @@
+import { motion } from 'framer-motion';
+
+import { circlesAnimation } from '~/animations/home';
+
 const Circles = () => {
   return (
-    <div className='relative flex justify-center items-center'>
+    <motion.div
+      animate='show'
+      className='relative flex justify-center items-center'
+      initial='hidden'
+      variants={circlesAnimation}
+    >
       <div className='mt-52 absolute h-[200px] w-[200px] border border-[#333] rounded-full animate-ping' />
       <div className='mt-52 absolute h-[300px] w-[300px] border border-[#333] rounded-full' />
       <div className='mt-52 absolute h-[500px] w-[500px] border border-[#333] rounded-full' />
       <div className='mt-52 absolute h-[650px] w-[650px] border border-[#f7ab0a] rounded-full animate-pulse' />
       <div className='mt-52 absolute h-[800px] w-[800px] border border-[#333] rounded-full' />
-    </div>
+    </motion.div>
   );
 };
 
