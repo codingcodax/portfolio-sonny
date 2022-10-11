389203ed5f44415f47106e8577bf0e2936cbec6d
Alexis Guzman <alexis12989x@gmail.com>
2022-10-11 17:44:31 -0500

    feat(pages): import about section into home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import type { NextPage } from 'next';
 
 import { Header } from '~/components/layout';
-import { Hero } from '~/components/home';
+import { Hero, About } from '~/components/home';
 
 const Home: NextPage = () => {
   return (
     <div className='h-screen text-white bg-[rgb(36,36,36)] snap-y snap-mandatory overflow-scroll z-0'>
       <Header />
       <Hero />
+      <About />
     </div>
   );
 };
