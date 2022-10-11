b356dd3f4879dca803c5d10fde61b4c5ab6f7f5d
Alexis Guzman <alexis12989x@gmail.com>
2022-10-11 10:44:02 -0500

    feat(hero): setup circles component

--- src/components/home/hero/Circles.tsx
+++ src/components/home/hero/Circles.tsx
@@ -1,7 +1,11 @@
 const Circles = () => {
   return (
-    <div>
-      <p>Circles Component</p>
+    <div className='relative flex justify-center items-center'>
+      <div className='mt-52 absolute h-[200px] w-[200px] border border-[#333] rounded-full animate-ping' />
+      <div className='mt-52 absolute h-[300px] w-[300px] border border-[#333] rounded-full' />
+      <div className='mt-52 absolute h-[500px] w-[500px] border border-[#333] rounded-full' />
+      <div className='mt-52 absolute h-[650px] w-[650px] border border-[#f7ab0a] rounded-full animate-pulse' />
+      <div className='mt-52 absolute h-[800px] w-[800px] border border-[#333] rounded-full' />
     </div>
   );
 };
