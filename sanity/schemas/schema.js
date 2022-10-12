d68a9d3ad5703c592181f064548fc099f2105980
Alexis Guzman <alexis12989x@gmail.com>
2022-10-12 12:50:44 -0500

    style(sanity): format schema file

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -1,12 +1,10 @@
-import createSchema from 'part:@sanity/base/schema-creator'
+import createSchema from 'part:@sanity/base/schema-creator';
 
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+import schemaTypes from 'all:part:@sanity/base/schema-type';
 
-import pageInfo from './pageInfo'
+import pageInfo from './pageInfo';
 
 export default createSchema({
   name: 'default',
-  types: schemaTypes.concat([
-    pageInfo,
-  ]),
-})
+  types: schemaTypes.concat([pageInfo]),
+});
