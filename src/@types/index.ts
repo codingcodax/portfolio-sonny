d0f750d9e858ef7eb4d041d339e574bbd9c4c93a
Alexis Guzman <alexis12989x@gmail.com>
2022-10-13 14:10:55 -0500

    feat(types): export types
    Export Image, Page Info, Project, Sanity Body, Skill and Social types

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,1 +1,7 @@
 export type { default as IconType } from './icon';
+export type { default as ImageType } from './image';
+export type { default as PageInfoType } from './pageInfo';
+export type { default as ProjectType } from './project';
+export type { default as SanityBodyType } from './sanityBody';
+export type { default as SkillType } from './skill';
+export type { default as SocialType } from './social';
