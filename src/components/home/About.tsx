810f18d46ae3cf897d178ed238056466d6b9dfce
Alexis Guzman <alexis12989x@gmail.com>
2022-10-18 17:06:17 -0500

    fix(home): update styles to remove absolute

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -13,41 +13,43 @@ interface AboutProps {
 const About: FC<AboutProps> = ({ pageInfo: { bgInfo, name, profilePic } }) => {
   return (
     <motion.section
-      className='px-10 mx-auto h-screen max-w-7xl relative flex justify-evenly items-center flex-col md:flex-row text-center md:text-left snap-center'
+      className='px-10 mx-auto h-screen max-w-7xl snap-start'
       id='about'
       initial={{ opacity: 0 }}
       transition={{ duration: 1.5 }}
       whileInView={{ opacity: 1 }}
     >
-      <h2 className='absolute top-24 text-gray-500 text-2xl uppercase tracking-[20px]'>
+      <h2 className='pt-20 mb-20 text-gray-500 text-center text-2xl uppercase tracking-[20px]'>
         About
       </h2>
 
-      <motion.div
-        className='-mb-20 md:mb-0 h-56 md:h-95 xl:h-[600px] w-56 md:w-64 xl:w-[500px] flex-shrink-0'
-        initial='hidden'
-        variants={aboutImageAnimation}
-        viewport={{ once: true }}
-        whileInView='whileInView'
-      >
-        <Image
-          alt={name}
-          className='rounded-full md:rounded-lg'
-          height={500}
-          objectFit='cover'
-          src={urlFor(profilePic).url()}
-          width={500}
-        />
-      </motion.div>
+      <div className='flex justify-evenly items-center flex-col md:flex-row text-center md:text-left '>
+        <motion.div
+          className='mb-14 h-56 md:h-95 xl:h-auto w-56 md:w-64 xl:w-[500px] shrink-0'
+          initial='hidden'
+          variants={aboutImageAnimation}
+          viewport={{ once: true }}
+          whileInView='whileInView'
+        >
+          <Image
+            alt={name}
+            className='rounded-full md:rounded-lg'
+            height={500}
+            objectFit='cover'
+            src={urlFor(profilePic).url()}
+            width={500}
+          />
+        </motion.div>
 
-      <div className='px-0 md:px-10'>
-        <h4 className='mb-10 text-4xl font-semibold'>
-          Here is a{' '}
-          <span className='underline decoration-[#f7ab0a]/50'>little</span>{' '}
-          background
-        </h4>
+        <div className='px-0 md:px-10'>
+          <h4 className='mb-8 text-4xl font-semibold'>
+            Here is a{' '}
+            <span className='underline decoration-[#f7ab0a]/50'>little</span>{' '}
+            background
+          </h4>
 
-        <p className='mb-4'>{bgInfo}</p>
+          <p className='mb-4'>{bgInfo}</p>
+        </div>
       </div>
     </motion.section>
   );
