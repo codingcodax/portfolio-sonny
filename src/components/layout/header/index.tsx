4717e33ca201e2b0fa880c9ac71cb57accd6b57b
Alexis Guzman <alexis12989x@gmail.com>
2022-10-18 16:10:34 -0500

    feat(header): pass social props to nav and use email prop

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,18 +1,25 @@
+import { FC } from 'react';
 import { motion } from 'framer-motion';
 
+import { PageInfoType, SocialType } from '~/@types';
 import { comingLeft } from '~/animations/header';
 import { Link } from '~/components';
 import { MailIcon } from '~/components/icons';
 
 import Nav from './Nav';
 
-const Header = () => {
+interface HeaderProps {
+  socials: SocialType[];
+  email: PageInfoType['email'];
+}
+
+const Header: FC<HeaderProps> = ({ socials, email }) => {
   return (
     <header className='p-5 mx-auto max-w-7xl sticky top-0 flex justify-between'>
-      <Nav />
+      <Nav socials={socials} />
 
       <motion.div animate='show' initial='hidden' variants={comingLeft}>
-        <Link className='flex items-center gap-2' href='/'>
+        <Link className='flex items-center gap-2' href={`mailto:${email}`}>
           <MailIcon className='h-6 w-6 stroke-gray-400' />
           <span className='hidden uppercase text-sm text-gray-400 md:inline-block'>
             Get In Touch
