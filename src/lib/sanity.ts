ad2fa6fd525f129685c460cb3b2b79d9b2384f8f
Alexis Guzman <alexis12989x@gmail.com>
2022-10-18 16:31:52 -0500

    fix(lib): convert spread env values to normal const

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -2,8 +2,9 @@ import { createClient } from 'next-sanity';
 import createImageUrlBuilder from '@sanity/image-url';
 import type { SanityImageSource } from '@sanity/image-url/lib/types/types';
 
-const { NEXT_PUBLIC_SANITY_DATASET, NEXT_PUBLIC_SANITY_PROJECT_ID, NODE_ENV } =
-  process.env;
+const NEXT_PUBLIC_SANITY_DATASET = process.env.NEXT_PUBLIC_SANITY_DATASET;
+const NEXT_PUBLIC_SANITY_PROJECT_ID = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const NODE_ENV = process.env.NODE_ENV;
 
 export const config = {
   dataset: NEXT_PUBLIC_SANITY_DATASET || 'production',
