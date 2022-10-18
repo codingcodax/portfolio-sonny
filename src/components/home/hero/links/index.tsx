4731ef7bb30394f429e60fe65b7b3ba39005c269
Alexis Guzman <alexis12989x@gmail.com>
2022-10-18 16:23:28 -0500

    feat(links): remove experience link

--- src/components/home/hero/links/index.tsx
+++ src/components/home/hero/links/index.tsx
@@ -4,7 +4,6 @@ const Links = () => {
   return (
     <div className='flex justify-center gap-x-2'>
       <Item href='#about'>About</Item>
-      <Item href='#experience'>Experience</Item>
       <Item href='#skills'>Skills</Item>
       <Item href='#projects'>Projects</Item>
     </div>
