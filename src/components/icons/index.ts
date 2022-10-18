da24254026d7a7e5a85abb6ff423ea2771d4204d
Alexis Guzman <alexis12989x@gmail.com>
2022-10-18 16:02:48 -0500

    feat(icons): export linkedin icon

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,5 +1,6 @@
 export { default as FacebookIcon } from './Facebook';
 export { default as InstagramIcon } from './Instagram';
+export { default as LinkedinIcon } from './Linkedin';
 export { default as MailIcon } from './Mail';
 export { default as ShareIcon } from './Share';
 export { default as TwitterIcon } from './Twitter';
