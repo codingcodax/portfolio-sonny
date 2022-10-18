9d0707f00485bc28b69ce823d3d8c1ffeef3f080
Alexis Guzman <alexis12989x@gmail.com>
2022-10-18 13:03:33 -0500

    feat(lib): create sanity client

---
+++ src/lib/sanity.ts
@@ -0,0 +1,18 @@
+import { createClient } from 'next-sanity';
+import createImageUrlBuilder from '@sanity/image-url';
+import type { SanityImageSource } from '@sanity/image-url/lib/types/types';
+
+const { NEXT_PUBLIC_SANITY_DATASET, NEXT_PUBLIC_SANITY_PROJECT_ID, NODE_ENV } =
+  process.env;
+
+export const config = {
+  dataset: NEXT_PUBLIC_SANITY_DATASET || 'production',
+  projectId: NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  apiVersion: '2021-10-21',
+  useCdn: NODE_ENV === 'production',
+};
+
+export const sanityClient = createClient(config);
+
+export const urlFor = (source: SanityImageSource) =>
+  createImageUrlBuilder(config).image(source);
