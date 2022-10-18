d2a634447fa27bf5bcffc8dd53ec88451a0cf508
Alexis Guzman <alexis12989x@gmail.com>
2022-10-18 16:11:15 -0500

    feat(header): make a list of items with social props and social icons

--- src/components/layout/header/Nav.tsx
+++ src/components/layout/header/Nav.tsx
@@ -1,25 +1,32 @@
 import { motion } from 'framer-motion';
+import { FC } from 'react';
 
+import { SocialType } from '~/@types';
 import { comingRight } from '~/animations/header';
-import { InstagramIcon, TwitterIcon, YoutubeIcon } from '~/components/icons';
+import socialIcons from '~/data/socialIcons';
 
 import Item from './Item';
 
-const Nav = () => {
+interface NavProps {
+  socials: SocialType[];
+}
+
+const Nav: FC<NavProps> = ({ socials }) => {
+  const navItems = socials.map((social) => ({
+    ...social,
+    // @ts-ignore
+    icon: socialIcons.find((socialIcon) => socialIcon.name === social.title)
+      .icon,
+  }));
+
   return (
     <motion.nav animate='show' initial='hidden' variants={comingRight}>
       <ul className='flex gap-4'>
-        <Item href='https://www.youtube.com/channel/UCMY0GhV1HuX4XdbgalC77VQ'>
-          <YoutubeIcon className='h-6 w-6 stroke-gray-400' />
-        </Item>
-
-        <Item href='https://www.instagram.com/codingcodax/'>
-          <InstagramIcon className='h-6 w-6 stroke-gray-400' />
-        </Item>
-
-        <Item href='https://twitter.com/codingcodax'>
-          <TwitterIcon className='h-6 w-6 stroke-gray-400' />
-        </Item>
+        {navItems.map((navItem) => (
+          <Item key={navItem._id} href={navItem.url}>
+            {navItem.icon}
+          </Item>
+        ))}
       </ul>
     </motion.nav>
   );
