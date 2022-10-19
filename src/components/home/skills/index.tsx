e700c5143ba201ad99f6b6cb9266651d7fff6082
Alexis Guzman <alexis12989x@gmail.com>
2022-10-19 16:15:56 -0500

    feat(home): setup skills component

--- src/components/home/skills/index.tsx
+++ src/components/home/skills/index.tsx
@@ -1,8 +1,28 @@
-const Skills = () => {
+import { FC } from 'react';
+
+import { SkillType } from '~/@types';
+
+import Item from './Item';
+
+interface SkillsProps {
+  skills: SkillType[];
+}
+
+const Skills: FC<SkillsProps> = ({ skills }) => {
+  console.log(skills);
+
   return (
-    <div>
-      <p>Skills Section</p>
-    </div>
+    <section className='px-10 mx-auto h-screen max-w-7xl snap-start'>
+      <h2 className='pt-20 mb-20 text-gray-500 text-center text-2xl uppercase tracking-[20px]'>
+        Skills
+      </h2>
+
+      <div className='mx-auto w-fit grid grid-cols-4 gap-6 justify-center'>
+        {skills.map((skill) => (
+          <Item key={skill._id} skill={skill} />
+        ))}
+      </div>
+    </section>
   );
 };
 
