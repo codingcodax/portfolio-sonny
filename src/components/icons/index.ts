4b21a963207db0af6e531ec8e34b61a0db15afe5
Alexis Guzman <alexis12989x@gmail.com>
2022-10-20 12:38:57 -0500

    feat(icons): export map icon

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -2,6 +2,7 @@ export { default as FacebookIcon } from './Facebook';
 export { default as InstagramIcon } from './Instagram';
 export { default as LinkedinIcon } from './Linkedin';
 export { default as MailIcon } from './Mail';
+export { default as MapIcon } from './Map';
 export { default as ShareIcon } from './Share';
 export { default as TwitterIcon } from './Twitter';
 export { default as YoutubeIcon } from './Youtube';
