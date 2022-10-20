e858c2a19f06766f4bb3b9e32671bfebbf6bbda6
Alexis Guzman <alexis12989x@gmail.com>
2022-10-20 13:19:41 -0500

    feat(contact): setup item component

--- src/components/home/contact/Item.tsx
+++ src/components/home/contact/Item.tsx
@@ -1,7 +1,15 @@
-const Item = () => {
+import { FC, ReactNode } from 'react';
+
+interface ItemProps {
+  content: string;
+  children: ReactNode;
+}
+
+const Item: FC<ItemProps> = ({ content, children }) => {
   return (
-    <div>
-      <p>Item Component</p>
+    <div className='flex items-center space-x-4'>
+      {children}
+      <p>{content}</p>
     </div>
   );
 };
