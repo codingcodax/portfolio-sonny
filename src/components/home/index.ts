6ee8d31e3b91d6e3124002159a11d11cffa1a706
Alexis Guzman <alexis12989x@gmail.com>
2022-10-20 10:57:24 -0500

    feat(home): export contact section

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -2,3 +2,4 @@ export { default as Hero } from './hero';
 export { default as About } from './About';
 export { default as Skills } from './skills';
 export { default as Projects } from './projects';
+export { default as Contact } from './contact';
