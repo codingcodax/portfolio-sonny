cc497077065fdd8e78c93f07b9b76ecce83d5e9f
Alexis Guzman <alexis12989x@gmail.com>
2022-10-20 13:19:24 -0500

    feat(contact): setup form component

--- src/components/home/contact/Form.tsx
+++ src/components/home/contact/Form.tsx
@@ -1,7 +1,26 @@
 const Form = () => {
   return (
-    <div>
-      <p>Form Component</p>
+    <div className='grid grid-cols-2 gap-2'>
+      <input className='contact-input' placeholder='Name' type='text' />
+      <input className='contact-input' placeholder='Email' type='email' />
+      <input
+        className='contact-input col-span-2'
+        placeholder='Subject'
+        type='text'
+      />
+      <textarea
+        className='contact-input col-span-2 max-h-36 h-20 min-h-16 resize-y'
+        id='message'
+        name='message'
+        placeholder='Message'
+      />
+
+      <button
+        className='py-5 px-10 col-span-2 text-black text-lg font-bold bg-[#f7ab0a] rounded-md'
+        type='submit'
+      >
+        Submit
+      </button>
     </div>
   );
 };
