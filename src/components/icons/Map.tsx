aba96608e2aef916b2375f42ed72390384f8c51f
Alexis Guzman <alexis12989x@gmail.com>
2022-10-20 12:37:14 -0500

    feat(icons): create map icon

---
+++ src/components/icons/Map.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react';
+
+import type { IconType } from '~/@types';
+
+const Map: FC<IconType> = ({ className }) => {
+  return (
+    <svg
+      className={className}
+      fill='none'
+      stroke='currentColor'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth='2'
+      viewBox='0 0 24 24'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path d='M0 0h24v24H0z' fill='none' stroke='none' />
+      <circle cx='12' cy='11' r='3' />
+      <path d='M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z' />
+    </svg>
+  );
+};
+
+export default Map;
